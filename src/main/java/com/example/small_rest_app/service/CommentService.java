ca6e2632cb1bfad90dfce045fb55e02676a20520
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-03-19 11:37:14 +0300

    CommentService created and some logic designed was implemented

--- src/main/java/com/example/small_rest_app/service/CommentService.java
+++ src/main/java/com/example/small_rest_app/service/CommentService.java
@@ -1,4 +1,16 @@
 package com.example.small_rest_app.service;
 
+import com.example.small_rest_app.dto.CommentResponseDto;
+import com.example.small_rest_app.entity.Comment;
+import com.example.small_rest_app.entity.Post;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentService {
+    List<Comment> findAll(Optional<Long> userId);
+    Comment save(CommentResponseDto comment);
+    Comment delete(Long id);
+    Comment findById(Long id);
+
 }
