ecfccb84952a373e7d75afaf09cca441a15e1b13
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-03-19 15:54:49 +0300

    CommentRepository created and some custom methods added

--- src/main/java/com/example/small_rest_app/repository/CommentRepository.java
+++ src/main/java/com/example/small_rest_app/repository/CommentRepository.java
@@ -3,5 +3,10 @@ package com.example.small_rest_app.repository;
 import com.example.small_rest_app.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+    List<Comment> findByUserId(Long userId);
+    List<Comment> findByPostId(Long postId);
+    List<Comment> findByPostIdAndUserId(Long userId,Long postId);
 }
