63b67459fd21f9ba5739c7d3c0ee8cddbb0fba7d
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-03-19 16:20:16 +0300

    LikeRepository created and some custom methods implemented

--- src/main/java/com/example/small_rest_app/repository/LikeRepository.java
+++ src/main/java/com/example/small_rest_app/repository/LikeRepository.java
@@ -3,5 +3,10 @@ package com.example.small_rest_app.repository;
 import com.example.small_rest_app.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like,Long> {
+    List<Like> findByPostIdAndUserId(Long userId, Long postId);
+    List<Like> findByPostId(Long postId);
+    List<Like> findByUserId(Long userId);
 }
