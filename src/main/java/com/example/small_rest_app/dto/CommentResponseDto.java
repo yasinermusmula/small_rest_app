456e64555da4f6f0e6530b9cd6ca9a00e06b533a
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-03-19 11:50:33 +0300

    Created CommentResponseDto and CommentUpdateReq

---
+++ src/main/java/com/example/small_rest_app/dto/CommentResponseDto.java
@@ -0,0 +1,8 @@
+package com.example.small_rest_app.dto;
+
+import lombok.Data;
+
+@Data
+public class CommentResponseDto {
+    private String text;
+}
---
+++ src/main/java/com/example/small_rest_app/dto/CommentUpdateReq.java
@@ -0,0 +1,8 @@
+package com.example.small_rest_app.dto;
+
+import lombok.Data;
+
+@Data
+public class CommentUpdateReq {
+    private String text;
+}
