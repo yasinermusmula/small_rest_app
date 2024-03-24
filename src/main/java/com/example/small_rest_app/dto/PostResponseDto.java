ed09e8db4d26a1fe1df58fe22dd209c9de385054
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-03-24 17:58:13 +0300

    What we will return for the response object is specified.

--- src/main/java/com/example/small_rest_app/dto/PostResponseDto.java
+++ src/main/java/com/example/small_rest_app/dto/PostResponseDto.java
@@ -1,11 +1,24 @@
 package com.example.small_rest_app.dto;
 
+import com.example.small_rest_app.entity.Post;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+
 public class PostResponseDto {
     private Long id;
+    private Long userId;
+    private String userName;
     private String text;
     private String title;
-    private Long userId;
+
+    public PostResponseDto(Post entity){
+        this.id = entity.getId();
+        this.userId=entity.getUser().getId();
+        this.userName = entity.getUser().getUserName();
+        this.text=entity.getText();
+        this.title=entity.getTitle();
+    }
 }
